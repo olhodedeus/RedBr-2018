9961e34fc127466e35cfa37151e9b81ebcf48702
Olho de Deus <portaltelezap@gmail.com>
2018-03-18 13:32:39 -0400

    Update banhammer.lua

--- lua/plugins/banhammer.lua
+++ lua/plugins/banhammer.lua
@@ -13,33 +13,53 @@ local function markup_tempban(chat_id, user_id, time_value)
 
 	local markup = {inline_keyboard={
 		{--first line
-			{text = '-', callback_data = ('tempban:val:m:%s:%s'):format(user_id, chat_id)},
+			{text = '➖', callback_data = ('tempban:val:m:%s:%s'):format(user_id, chat_id)},
 			{text = '🕑 '..time_value, callback_data = 'tempban:nil'},
-			{text = '+', callback_data = ('tempban:val:p:%s:%s'):format(user_id, chat_id)}
+			{text = '➕', callback_data = ('tempban:val:p:%s:%s'):format(user_id, chat_id)}
 		},
 		{--second line
-			{text = 'minutes', callback_data = ('tempban:ban:m:%s:%s'):format(user_id, chat_id)},
-			{text = 'hours', callback_data = ('tempban:ban:h:%s:%s'):format(user_id, chat_id)},
-			{text = 'days', callback_data = ('tempban:ban:d:%s:%s'):format(user_id, chat_id)},
+			{text = i18n('minutes'), callback_data = ('tempban:ban:m:%s:%s'):format(user_id, chat_id)},
+			{text = i18n('hours'), callback_data = ('tempban:ban:h:%s:%s'):format(user_id, chat_id)},
+			{text = i18n('days'), callback_data = ('tempban:ban:d:%s:%s'):format(user_id, chat_id)},
 		}
 	}}
 
 	return markup
 end
+local function markup_tempmute(chat_id, user_id, time_value)
+	local key = ('chat:%s:%s:tmutevalue'):format(chat_id, user_id)
+	time_value = time_value or (db:get(key) or 3)
 
+	local markup = {inline_keyboard={
+		{--first line
+			{text = '➖', callback_data = ('tempmute:val:m:%s:%s'):format(user_id, chat_id)},
+			{text = '🕑 '..time_value, callback_data = 'tempmute:nil'},
+			{text = '➕', callback_data = ('tempmute:val:p:%s:%s'):format(user_id, chat_id)}
+		},
+		{--second line
+			{text = i18n('minutes'), callback_data = ('tempmute:mute:m:%s:%s'):format(user_id, chat_id)},
+			{text = i18n('hours'), callback_data = ('tempmute:mute:h:%s:%s'):format(user_id, chat_id)},
+			{text = i18n('days'), callback_data = ('tempmute:mute:d:%s:%s'):format(user_id, chat_id)},
+		}
+	}}
+
+	return markup
+end
 local function get_motivation(msg)
 	if msg.reply then
 		return msg.text:match(("%sban (.+)"):format(config.cmd))
 			or msg.text:match(("%skick (.+)"):format(config.cmd))
 			or msg.text:match(("%smute (.+)"):format(config.cmd))
+			or msg.text:match(("%sdesmute (.+)"):format(config.cmd))
 			or msg.text:match(("%stempban .+\n(.+)"):format(config.cmd))
+			or msg.text:match(("%stempmute .+\n(.+)"):format(config.cmd))
 	else
-		if msg.text:find(config.cmd.."ban @%w[%w_]+ ") or msg.text:find(config.cmd.."kick @%w[%w_]+ ") then
-			return msg.text:match(config.cmd.."ban @%w[%w_]+ (.+)") or msg.text:match(config.cmd.."kick @%w[%w_]+ (.+)")
-		elseif msg.text:find(config.cmd.."ban %d+ ") or msg.text:find(config.cmd.."kick %d+ ") then
-			return msg.text:match(config.cmd.."ban %d+ (.+)") or msg.text:match(config.cmd.."kick %d+ (.+)")
+		if msg.text:find(config.cmd.."ban @%w[%w_]+ ") or msg.text:find(config.cmd.."mute @%w[%w_]+ ") or msg.text:find(config.cmd.."kick @%w[%w_]+ ") then
+			return msg.text:match(config.cmd.."ban @%w[%w_]+ (.+)") or msg.text:match(config.cmd.."mute @%w[%w_]+ (.+)") or msg.text:match(config.cmd.."kick @%w[%w_]+ (.+)")
+		elseif msg.text:find(config.cmd.."ban %d+ ") or msg.text:find(config.cmd.."mute %d+ ") or msg.text:find(config.cmd.."kick %d+ ") then
+			return msg.text:match(config.cmd.."ban %d+ (.+)") or msg.text:match(config.cmd.."mute %d+ (.+)") or msg.text:match(config.cmd.."kick %d+ (.+)")
 		elseif msg.entities then
-			return msg.text:match(config.cmd.."ban .+\n(.+)") or msg.text:match(config.cmd.."kick .+\n(.+)")
+			return msg.text:match(config.cmd.."ban .+\n(.+)") or msg.text:match(config.cmd.."mute .+\n(.+)") or msg.text:match(config.cmd.."kick .+\n(.+)")
 		end
 	end
 end
@@ -72,6 +92,20 @@ function plugin.onTextMessage(msg, blocks)
 
 				local markup = markup_tempban(msg.chat.id, user_id)
 				api.sendReply(msg, i18n('Use -/+ to edit the value, then select a timeframe to temporary ban the user'), nil,
+					markup)
+			end
+			if blocks[1] == 'tempmute' then
+				local time_value = msg.text:match(("%stempmute.*\n"):format(config.cmd).."(%d+)")
+				if time_value then --save the time value passed by the user
+					if tonumber(time_value) > 100 then
+						time_value = 100
+					end
+					local key = ('chat:%s:%s:tmutevalue'):format(msg.chat.id, user_id)
+					db:setex(key, 3600, time_value)
+				end
+
+				local markup = markup_tempmute(msg.chat.id, user_id)
+				api.sendReply(msg, i18n('Use -/+ to edit the value, then select a timeframe to temporary mute the user'), nil,
 					markup)
 			end
 			if blocks[1] == 'kick' or blocks[1] == 'remover' then
@@ -187,9 +221,40 @@ function plugin.onCallbackQuery(msg, matches)
 					db:setex(key, 3600, new_value)
 				end
 			end
-
-			local markup = markup_tempban(msg.chat.id, user_id, new_value)
+				local markup = markup_tempban(msg.chat.id, user_id, new_value)
+				local markup = markup_tempmute(msg.chat.id, user_id, new_value)
 			api.editMessageReplyMarkup(msg.chat.id, msg.message_id, markup)
+		elseif matches[1] == 'mute' then
+			local user_id = matches[3]
+			local key = ('chat:%d:%s:tmutevalue'):format(msg.chat.id, user_id)
+			local time_value = tonumber(db:get(key) or 3)
+			local timeframe_string, until_date
+			if matches[2] == 'h' then
+				time_value = time_value <= 24 and time_value or 24
+				timeframe_string = i18n('hours')
+				until_date = msg.date + (time_value * 3600)
+			elseif matches[2] == 'd' then
+				time_value = time_value <= 30 and time_value or 30
+				timeframe_string = i18n('days')
+				until_date = msg.date + (time_value * 3600 * 24)
+			elseif matches[2] == 'm' then
+				time_value = time_value <= 60 and time_value or 60
+				timeframe_string = i18n('minutes')
+				until_date = msg.date + (time_value * 60)
+			end
+			local res, _, motivation = api.tempmuteUser(msg.chat.id, user_id, until_date)
+			if not res then
+				motivation = motivation or i18n("I can't kick this user.\n"
+					.. "I am not allowed to ban or the target user is an admin")
+				api.editMessageText(msg.chat.id, msg.message_id, motivation)
+			else
+				local text = i18n("User muted for %d %s"):format(time_value, timeframe_string)
+				api.editMessageText(msg.chat.id, msg.message_id, text)
+				db:del(key)
+				end
+			
+			
+		--	api.editMessageReplyMarkup(msg.chat.id, msg.message_id, markup)
 		elseif matches[1] == 'ban' then
 			local user_id = matches[3]
 			local key = ('chat:%d:%s:tbanvalue'):format(msg.chat.id, user_id)
@@ -230,16 +295,16 @@ plugin.triggers = {
 		config.cmd..'(remover)$',
 		config.cmd..'(mute) (.+)',
 		config.cmd..'(mute)$',
-		config.cmd..'(mudo) (.+)',
-		config.cmd..'(mudo)$',
-		config.cmd..'(silenciar) (.+)',
-		config.cmd..'(silenciar)$',
 		config.cmd..'(desmute) (.+)',
 		config.cmd..'(desmute)$',
 		config.cmd..'(unmute)$',
 		config.cmd..'(unmute) (.+)',
 		config.cmd..'(livre)$',
 		config.cmd..'(livre) (.+)',
+		config.cmd..'(mudo) (.+)',
+		config.cmd..'(mudo)$',
+		config.cmd..'(silenciar) (.+)',
+		config.cmd..'(silenciar)$',
 		config.cmd..'(ban) (.+)',
 		config.cmd..'(ban)$',
 		config.cmd..'(banir) (.+)',
@@ -247,6 +312,8 @@ plugin.triggers = {
 		config.cmd..'(fwdban)$',
 		config.cmd..'(tempban)$',
 		config.cmd..'(tempban) (.+)',
+		config.cmd..'(tempmute)$',
+		config.cmd..'(tempmute) (.+)',
 		config.cmd..'(unban) (.+)',
 		config.cmd..'(unban)$',
 		config.cmd..'(desbanir) (.+)',
@@ -255,7 +322,10 @@ plugin.triggers = {
 	onCallbackQuery = {
 		'^###cb:tempban:(val):(%a):(%d+):(-%d+)',
 		'^###cb:tempban:(ban):(%a):(%d+):(-%d+)',
-		'^###cb:tempban:(nil)$'
+		'^###cb:tempban:(nil)$',
+		'^###cb:tempmute:(val):(%a):(%d+):(-%d+)',
+		'^###cb:tempmute:(mute):(%a):(%d+):(-%d+)',
+		'^###cb:tempmute:(nil)$'
 	}
 }
 
